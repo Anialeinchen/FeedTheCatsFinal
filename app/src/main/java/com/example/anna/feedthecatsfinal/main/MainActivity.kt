a5c10b22f7ef7c828a366281d70eabf4e2f118fa
Anna Morgiel <amorgiel@gmail.com>
2018-08-16 00:25:17 +0200

    add menu with adding new shift, display dialog, fetch value and parse it to long

--- app/src/main/java/com/example/anna/feedthecatsfinal/main/MainActivity.kt
+++ app/src/main/java/com/example/anna/feedthecatsfinal/main/MainActivity.kt
@@ -1,34 +1,61 @@
 package com.example.anna.feedthecatsfinal.main
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.example.anna.feedthecatsfinal.CatsFeedingEvent
 import com.example.anna.feedthecatsfinal.R
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
+import android.content.DialogInterface
+import android.support.v7.app.AlertDialog
+import android.widget.EditText
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    private val dateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+
     private var events: HashMap<String, CatsFeedingEvent> = HashMap()
 
-    private lateinit var dbRef : DatabaseReference
+    private lateinit var dbRef: DatabaseReference
 
     private lateinit var eventsRecyclerView: RecyclerView
-    private var eventAdadapter:EventsAdapter = EventsAdapter()
+    private var eventAdadapter: EventsAdapter = EventsAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupFirebaseDb()
-        addDummyEvent()
+        addNewEvent(123)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.getItemId()
+
+        when (id) {
+            R.id.newShift -> {
+                showChangeLangDialog()
+                return true
+            }
+
+        //similarly write for other actions
+
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
     }
 
     private fun setupFirebaseDb() {
@@ -63,17 +90,40 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun addDummyEvent() {
+    fun addNewEvent(date: Long) {
         val key = dbRef.push().key
         val event = CatsFeedingEvent().apply {
-            date = 123
+            this.date = date
             state = 0
-            type= 0
+            type = 0
         }
         dbRef.updateChildren(mapOf(Pair(key, event)))
     }
 
     companion object {
         private const val TABLE = "event"
+    }
+
+    fun showChangeLangDialog() {
+        val dialogBuilder = AlertDialog.Builder(this)
+        val inflater = this.layoutInflater
+        val dialogView = inflater.inflate(R.layout.custom_dialog, null)
+        dialogBuilder.setView(dialogView)
+
+        val edt = dialogView.findViewById(R.id.edit1) as EditText
+
+        dialogBuilder.setTitle("Custom dialog")
+        dialogBuilder.setMessage("Enter text below")
+        dialogBuilder.setPositiveButton("Done", DialogInterface.OnClickListener { dialog, whichButton ->
+            //do something with edt.getText().toString();
+            val nextShift = edt.text.toString()
+            val timestampNextShift = dateFormat.parse(nextShift).time
+            addNewEvent(timestampNextShift)
+        })
+        dialogBuilder.setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, whichButton ->
+            //pass
+        })
+        val b = dialogBuilder.create()
+        b.show()
     }
 }
